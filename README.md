9e4d6ceb0a460540a58cf76caa6bb8bc9c405c20
Abhishek Routh <routhabhishek6@gmail.com>
2025-10-14 18:07:18 +0530

    Added logging and 3 new endpoints

--- README.md
+++ README.md
@@ -1,13 +1,22 @@
-# Medicine Chatbot
+# Medicine Chatbot & Other AI Tasks
 
 This is a medicine chatbot built using Flask, Streamlit and Gemini API.
-It helps in educating people about medicines.
+It helps in educating people about medicines and also perform other AI tasks.
 
 ## Features 
 
 * system_prompt: Prompt to generate the use and side effects of the medicine
 * ai_explain_prompt: Promot to get a summarize details of the medicine
 * similar_medicine: Prompt to get an alternative medicine details
+* generate_text_prompt: Prompt to generate paragraph
+* summarize_prompt:Prompt to summarize the paragraph
+* analyze_prompt: Prompt to analyze
+
+### Logging: 
+
+To log each incoming request for tracking. All the files are stored 
+in logs/app.log. Timestamp, Endpoint name and User prompt will be
+returned.
 
 ## API endpoints
 
@@ -15,7 +24,9 @@ It helps in educating people about medicines.
 * /medicine_details POST: Use and side effect of medicine
 * /ai_explain POST: Dosage, Instructions and timing
 * /similar_medicine: similar medicine name, use and difference
-
+* /generate_text: To generate a paragraph using a text input
+* /summarize: To summarize the given article
+* /analyze: To analyze and return the intent of the article
 
 ## Technologies used
 
@@ -34,3 +45,15 @@ It helps in educating people about medicines.
 ### 3. Alternative:
 
 ![alt text](images/alternative.png)
+
+### 4. Generate Text:
+
+![alt text](images/generate_text.png)
+
+### 5. Summarize:
+
+![alt text](images/summarize.png)
+
+### 6. Analyze:
+
+![alt text](images/analyze.png)
--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 import google.generativeai as genai
 from dotenv import load_dotenv
 import os
+import logging
 
 load_dotenv
 
@@ -9,6 +10,14 @@ app = Flask(__name__)
 
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 model = genai.GenerativeModel("gemini-2.5-flash")
+
+os.makedirs('logs', exist_ok=True)
+logging.basicConfig(
+    filename='logs/app.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
 
 system_prompt = (
             '''You are a medicine assistant. The user will ask information about a medicine.
@@ -43,6 +52,32 @@ similar_medicine_prompt = (
             Difference: [How is it different from the medicine?]
          ''')
 
+generate_text_prompt = ( 
+        '''You are a AI assistant. The user will provide you with input.   
+            You need to generate a short story based on the input.
+            Your reply must be in the below format:
+            Text: [story based on the input]
+            '''
+)
+
+summarize_prompt = ( 
+        '''You are a AI assistant. The user will provide you with input.
+           You should summareized the input in less than 100 words. 
+            Your reply must be in the below format:
+            [Summary]: [Summary of the input text] 
+            [Title]: [A title for the summary]
+            '''
+)
+
+analyze_prompt = ( 
+        '''You are a AI assistant. The user will provide you with input.
+            You need to analyze the input and provide details of the input and it's intent.
+            Your response should be less than 100 words.
+            Your reply must be in the below format:
+             [Details of the input and it's intent]
+            '''
+)
+
 @app.route('/home', methods = ['GET'])
 def home():
     return f"Welcome to medicine chatbot"
@@ -73,6 +108,52 @@ def similar():
     response = model.generate_content(full_prompt)
     answer = response.text
     return jsonify({"reply":answer})
+
+@app.route('/generate_text', methods =['POST'])
+def gen_text():
+    data = request.get_json()
+    user_prompt = data.get("user_prompt")
+    if not user_prompt:
+        return jsonify({
+            "status": "error",
+            "message": "Please enter a text"
+        }), 400
+    logging.info(f"Endpoint name: /generate_text | Prompt: {user_prompt}")
+    full_prompt = f"{generate_text_prompt}\nUser: {user_prompt}\nAssistant:"
+    response = model.generate_content(full_prompt)
+    answer = response.text
+    return jsonify({"reply":answer})
+
+@app.route('/summarize', methods =['POST'])
+def summarize():
+    data = request.get_json()
+    user_prompt = data.get("user_prompt")
+    if not user_prompt:
+        return jsonify({
+            "status": "error",
+            "message": "Please enter an article"
+        }), 400
+    logging.info(f"Endpoint name: /summarize | Prompt: {user_prompt}")
+    full_prompt = f"{summarize_prompt}\nUser: {user_prompt}\nAssistant:"
+    response = model.generate_content(full_prompt)
+    answer = response.text
+    return jsonify({"reply":answer})
+
+@app.route('/analyze', methods =['POST'])
+def analyze():
+    data = request.get_json()
+    user_prompt = data.get("user_prompt")
+    if not user_prompt:
+        return jsonify({
+            "status": "error",
+            "message": "Please enter an article"
+        }), 400
+    logging.info(f"Endpoint name: /analyze | Prompt: {user_prompt}")
+    full_prompt = f"{analyze_prompt}\nUser: {user_prompt}\nAssistant:"
+    response = model.generate_content(full_prompt)
+    answer = response.text
+    return jsonify({"reply":answer})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
